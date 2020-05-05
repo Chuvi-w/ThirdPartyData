83c43481dda71c92bba7ae24685c71cbdbafb783
Chuvi <chuvi.w@gmail.com>
2020-05-05 16:47:47 +0300

    Init GitUtils

---
+++ __UpdateGitUtils.cmake
@@ -0,0 +1,24 @@
+cmake_minimum_required(VERSION 2.8.7)
+#THIRD_PARTY_UPDATE_SCRIPT SKIP_UPDADE_GIT_UTILS
+
+if(NOT THIRD_PARTY_GIT_DIR)
+ set(THIRD_PARTY_GIT_DIR ${CMAKE_BINARY_DIR}/_ThirdPartyGit CACHE INTERNAL "" FORCE)
+endif()
+
+
+git_clone(
+        PROJECT_NAME    cmake_git_clone
+        GIT_URL         https://github.com/tschuchortdev/cmake_git_clone.git
+        GIT_BRANCH      master
+		DIRECTORY ${ThirdPartyCloneDir}
+		QUIET
+)
+
+if(cmake_git_clone_CLONE_RESULT)
+	file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/GitUtils.cmake)
+	file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/Utils.cmake)
+	file(COPY ${cmake_git_clone_SOURCE_DIR}/cmake/GitUtils.cmake DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${cmake_git_clone_SOURCE_DIR}/cmake/Utils.cmake DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${cmake_git_clone_SOURCE_DIR}/LICENSE.TXT DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	message(STATUS "cmake_git_clone updated")
+endif()

6868d06c3d67c6221c0259f5828f0711413516eb
Chuvi <chuvi.w@gmail.com>
2020-05-07 01:17:19 +0300

    Update

--- __UpdateCImg.cmake
+++ __UpdateCImg.cmake
@@ -6,3 +6,25 @@ if(NOT THIRD_PARTY_GIT_DIR)
 endif()
 
 
+git_clone(
+        PROJECT_NAME    CImgGit
+        GIT_URL         https://github.com/dtschump/CImg.git
+        GIT_BRANCH      develop
+        DIRECTORY ${THIRD_PARTY_GIT_DIR}
+		QUIET
+)
+set(CIMG_SrcDir "${PROJECT_SOURCE_DIR}/ThirdParty/CImg")
+if(CImgGit_CLONE_RESULT)
+    file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/CImg.h)
+	file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/Licence_CeCILL_V2-en.txt)
+	file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/Licence_CeCILL-C_V1-en.txt)
+	file(REMOVE_RECURSE ${CMAKE_CURRENT_LIST_DIR}/plugins)
+	file(GLOB CimgPlugins ${CImgGit_SOURCE_DIR}/plugins/*.h)
+	list(FILTER CimgPlugins EXCLUDE REGEX ".*loop_macros.h$")
+	#message("CimgPlugins=${CimgPlugins}")
+	file(COPY ${CImgGit_SOURCE_DIR}/CImg.h DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${CImgGit_SOURCE_DIR}/Licence_CeCILL_V2-en.txt DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${CImgGit_SOURCE_DIR}/Licence_CeCILL-C_V1-en.txt DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+    file(COPY ${CimgPlugins}/ DESTINATION ${CMAKE_CURRENT_LIST_DIR}/plugins/)
+	message(STATUS "CImg updated")
+endif()

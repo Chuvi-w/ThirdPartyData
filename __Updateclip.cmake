830212f080728699075a9952f55ffc705dea7c1a
Chuvi <chuvi.w@gmail.com>
2020-05-07 01:25:55 +0300

    Update

--- __Updateclip.cmake
+++ __Updateclip.cmake
@@ -6,3 +6,21 @@ if(NOT THIRD_PARTY_GIT_DIR)
 endif()
 
 
+git_clone(
+        PROJECT_NAME    ClipGit
+        GIT_URL         https://github.com/dacap/clip.git
+        GIT_BRANCH      master
+        DIRECTORY ${THIRD_PARTY_GIT_DIR}
+		QUIET
+)
+
+if(ClipGit_CLONE_RESULT)
+    file(GLOB CLipFiles ${ClipGit_SOURCE_DIR}/*.h ${ClipGit_SOURCE_DIR}/*.cpp ${ClipGit_SOURCE_DIR}/*.mm)
+    file(COPY ${ClipGit_SOURCE_DIR}/LICENSE.txt DESTINATION ${CMAKE_CURRENT_LIST_DIR}/)
+    file(COPY ${CLipFiles} DESTINATION ${CMAKE_CURRENT_LIST_DIR}/)
+    file(READ ${ClipGit_SOURCE_DIR}/CMakeLists.txt ClipCMakeText)
+    string(REPLACE "if(CLIP_EXAMPLES)" "if(FALSE)#CLIP_EXAMPLES" ClipCMakeText "${ClipCMakeText}")
+    string(REPLACE "if(CLIP_TESTS)" "if(FALSE)#CLIP_TESTS" ClipCMakeText "${ClipCMakeText}")
+    file(WRITE ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt "${ClipCMakeText}")
+	message(STATUS "CLip updated")
+endif()

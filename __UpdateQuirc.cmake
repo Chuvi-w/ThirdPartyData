658118796280f7293f3f5bb43fd9fb8cf7016547
Chuvi <chuvi.w@gmail.com>
2020-05-07 16:35:17 +0300

    Update

--- __UpdateQuirc.cmake
+++ __UpdateQuirc.cmake
@@ -5,4 +5,19 @@ if(NOT THIRD_PARTY_GIT_DIR)
  set(THIRD_PARTY_GIT_DIR /_ThirdPartyGit CACHE INTERNAL  FORCE)
 endif()
 
+file(REMOVE_RECURSE ${THIRD_PARTY_GIT_DIR}/QuircGit/)
 
+git_clone(
+        PROJECT_NAME    QuircGit
+        GIT_URL         https://github.com/dlbeer/quirc.git
+        GIT_BRANCH      master
+		DIRECTORY ${THIRD_PARTY_GIT_DIR}
+		QUIET
+)
+
+if(QuircGit_CLONE_RESULT)
+	file(REMOVE_RECURSE ${CMAKE_CURRENT_LIST_DIR}/lib/)	
+	file(COPY ${QuircGit_SOURCE_DIR}/lib DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${QuircGit_SOURCE_DIR}/LICENSE DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	message(STATUS "Quirc updated")
+endif()

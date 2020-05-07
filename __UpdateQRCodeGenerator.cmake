a93958a6d7cc4d7a2092d67d498f3ce5d52e6718
Chuvi <chuvi.w@gmail.com>
2020-05-07 15:51:45 +0300

    Update

--- __UpdateQRCodeGenerator.cmake
+++ __UpdateQRCodeGenerator.cmake
@@ -5,4 +5,24 @@ if(NOT THIRD_PARTY_GIT_DIR)
  set(THIRD_PARTY_GIT_DIR /_ThirdPartyGit CACHE INTERNAL  FORCE)
 endif()
 
+file(REMOVE_RECURSE ${THIRD_PARTY_GIT_DIR}/QRCodeGenGit/)
+git_clone(
+        PROJECT_NAME    QRCodeGenGit
+        GIT_URL         https://github.com/nayuki/QR-Code-generator.git
+        GIT_BRANCH      master
+		DIRECTORY ${THIRD_PARTY_GIT_DIR}
+		QUIET
+)
 
+if(QRCodeGenGit_CLONE_RESULT)
+	file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/QrCode.cpp)
+	file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/QrCode.hpp)
+	file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/QrCodeGeneratorDemo.cpp)
+	file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/QrCodeGeneratorWorker.cpp)
+	
+	file(COPY ${QRCodeGenGit_SOURCE_DIR}/cpp/QrCode.cpp DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${QRCodeGenGit_SOURCE_DIR}/cpp/QrCode.hpp DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${QRCodeGenGit_SOURCE_DIR}/cpp/QrCodeGeneratorDemo.cpp DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${QRCodeGenGit_SOURCE_DIR}/cpp/QrCodeGeneratorWorker.cpp DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	message(STATUS "QRCodeGenerator updated")
+endif()

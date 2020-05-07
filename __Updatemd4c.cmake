e2e86337ca55351e2290eed68b0ab0a4342bcb13
Chuvi <chuvi.w@gmail.com>
2020-05-07 15:43:09 +0300

    Update

--- __Updatemd4c.cmake
+++ __Updatemd4c.cmake
@@ -5,4 +5,21 @@ if(NOT THIRD_PARTY_GIT_DIR)
  set(THIRD_PARTY_GIT_DIR /_ThirdPartyGit CACHE INTERNAL  FORCE)
 endif()
 
+file(REMOVE_RECURSE ${THIRD_PARTY_GIT_DIR}/md4c_git/)
+git_clone(
+        PROJECT_NAME    md4c_git
+        GIT_URL         https://github.com/mity/md4c.git
+        GIT_BRANCH      master
+		DIRECTORY ${THIRD_PARTY_GIT_DIR}
+		QUIET
+)
 
+if(md4c_git_CLONE_RESULT)
+	file(REMOVE_RECURSE ${CMAKE_CURRENT_LIST_DIR}/md4c/)
+	file(REMOVE_RECURSE ${CMAKE_CURRENT_LIST_DIR}/md2html/)
+	file(REMOVE 	${CMAKE_CURRENT_LIST_DIR}/LICENSE.md)
+    file(COPY ${md4c_git_SOURCE_DIR}/LICENSE.md DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+    file(COPY ${md4c_git_SOURCE_DIR}/md4c DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+    file(COPY ${md4c_git_SOURCE_DIR}/md2html DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	message(STATUS "md4c updated")
+endif()

7c743513c938f9b0d3cfc1052edb99afbcc02a56
Chuvi <chuvi.w@gmail.com>
2020-05-12 19:01:55 +0300

    InitAsio

---
+++ __Updateasio.cmake
@@ -0,0 +1,26 @@
+cmake_minimum_required(VERSION 2.8.7)
+#THIRD_PARTY_UPDATE_SCRIPT SKIP_UPDADE_CImg
+
+if(NOT THIRD_PARTY_GIT_DIR)
+ set(THIRD_PARTY_GIT_DIR /_ThirdPartyGit CACHE INTERNAL  FORCE)
+endif()
+
+file(REMOVE_RECURSE ${THIRD_PARTY_GIT_DIR}/AsioGit/)
+git_clone(
+        PROJECT_NAME    AsioGit
+        GIT_URL         https://github.com/chriskohlhoff/asio.git
+        GIT_BRANCH      master
+        DIRECTORY ${THIRD_PARTY_GIT_DIR}
+		QUIET
+)
+
+if(AsioGit_CLONE_RESULT)
+    file(REMOVE ${CMAKE_CURRENT_LIST_DIR}/LICENSE_1_0.txt)
+	file(REMOVE_RECURSE ${CMAKE_CURRENT_LIST_DIR}/src)
+	file(REMOVE_RECURSE ${CMAKE_CURRENT_LIST_DIR}/include)
+	file(COPY ${AsioGit_CLONE_RESULT}/asio/include/ DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${AsioGit_CLONE_RESULT}/asio/LICENSE_1_0.txt DESTINATION ${CMAKE_CURRENT_LIST_DIR})
+	file(COPY ${AsioGit_CLONE_RESULT}/src/asio.cpp DESTINATION ${CMAKE_CURRENT_LIST_DIR}/src)
+	file(COPY ${AsioGit_CLONE_RESULT}/src/asio_ssl.cpp DESTINATION ${CMAKE_CURRENT_LIST_DIR}/src)
+	message(STATUS "ASIO updated")
+endif()

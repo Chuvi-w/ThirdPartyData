b75e77ec8bca4186f6776c2edcb0ccfe4d50a167
Chuvi <chuvi.w@gmail.com>
2020-08-28 17:30:15 +0300

    UpdateAll

---
+++ CImgAll.h
@@ -0,0 +1,13 @@
+ #include <vector>
+ #include <string>
+ #include <sstream>
+ #include <algorithm>
+ 
+#define cimg_plugin  "plugins/chlpca.h"
+#define cimg_plugin1 "plugins/draw_gradient.h"
+#define cimg_plugin2 "plugins/nlmeans.h"
+#define cimg_plugin3 "plugins/skeleton.h"
+#define cimg_plugin4 "plugins/tinymatwriter.h"
+#define cimg_plugin5 "plugins/vrml.h"
+#define cimg_plugin6 "plugins/vtk.h"
+#include "CImg.h"

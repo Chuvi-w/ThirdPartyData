930f9e7170d49687d93f4fd561cfcde79ef3fafd
Chuvi <chuvi.w@gmail.com>
2020-09-23 15:08:36 +0300

    pretty_stream

--- pub_pretty.hpp
+++ pub_pretty.hpp
@@ -19,4 +19,18 @@ namespace json_dto
 
 		return buffer.GetString();
 	}
+
+	template< typename Type >
+	void to_stream_p(std::ostream& to, const Type& type)
+	{
+		rapidjson::Document output_doc;
+		json_dto::json_output_t jout{
+			output_doc, output_doc.GetAllocator() };
+
+		jout << type;
+
+		rapidjson::OStreamWrapper wrapper{ to };
+		rapidjson::PrettyWriter< rapidjson::OStreamWrapper > writer{ wrapper };
+		output_doc.Accept(writer);
+	}
 }
